f418b640967d44622f48e75e2e9a12d073c725d5
Legitbub <damjustin25@gmail.com>
2026-04-10 13:11:32 -0700

    Added initial views

---
+++ src/main/java/com/example/graft/views/CreateAccountView.java
@@ -0,0 +1,32 @@
+package com.example.graft.views;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+@Route("newaccount")
+@AnonymousAllowed
+@PageTitle("New Account")
+public class CreateAccountView extends HorizontalLayout {
+    public CreateAccountView() {
+        var form = new VerticalLayout();
+        form.setJustifyContentMode(JustifyContentMode.CENTER);
+        add(form);
+        form.add(new H1("Welcome to Graft! Create your account."));
+        var username = new TextField();
+        username.setPlaceholder("Username");
+        var password = new TextField();
+        password.setPlaceholder("Password");
+        form.add(username);
+        form.add(password);
+    }
+
+    public static void show() {
+        UI.getCurrent().navigate(CreateAccountView.class);
+    }
+}
--- src/main/java/com/example/graft/views/MainView.java
+++ src/main/java/com/example/graft/views/MainView.java
@@ -2,15 +2,30 @@ package com.example.graft.views;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 @Route("")
 @AnonymousAllowed // public entry page
+@PageTitle("Graft - A Social Media Sim")
 public class MainView extends VerticalLayout {
     public MainView() {
-        add(new H1("Simulation Active"));
-        add(new Button("Check Neo4j Connection"));
+        add(new H1("Graft"));
+        var username = new TextField();
+        username.setPlaceholder("Username");
+        var password = new TextField();
+        password.setPlaceholder("Password");
+        add(username);
+        add(password);
+        var buttonBar = new HorizontalLayout();
+        buttonBar.add(new Button("Login"));
+        buttonBar.add(new Button("Create Account", event -> {
+            CreateAccountView.show();
+        }));
+        add(buttonBar);
     }
 }
